61d3eff9aed067db54945f9e922908c94eaa0bc3
Xuanzhuo Liu <122359062+LXZgogoDuck@users.noreply.github.com>
2025-07-30 18:57:31 +0800

    Update octo_inference.py

--- octo/model/octo_inference.py
+++ octo/model/octo_inference.py
@@ -113,19 +113,144 @@ class OctoInference:
         # pad_mask[:self.horizon - self.num_image_history] = 0
         return images, pad_mask
 
-    def reset(self, task_description: str) -> None:
-        self.task = self.model.create_tasks(texts=[task_description])
-        self.task_description = task_description
+     def reset(self, task_description: Optional[str] = None, goal_image: Optional[np.ndarray] = None) -> None:
         self.image_history.clear()
         if self.action_ensemble:
             self.action_ensembler.reset()
         self.num_image_history = 0
-
         self.sticky_action_is_on = False
         self.gripper_action_repeat = 0
         self.sticky_gripper_action = 0.0
-        # self.gripper_is_closed = False
         self.previous_gripper_action = None
+
+        # Handle task creation
+        goals = None
+        texts = None
+
+        if goal_image is not None:
+            resized_goal_image = self._resize_image(goal_image)
+            goals = {"image_primary": np.expand_dims(resized_goal_image, axis=0)}  # (1, H, W, 3)
+        if task_description is not None:
+            texts = [task_description]
+
+        self.task = self.model.create_tasks(goals=goals, texts=texts)
+        self.task_description = task_description
+        self.goal_image = goal_image
+
+    def step(self, image: np.ndarray, task_description: Optional[str] = None, goal_image: Optional[np.ndarray] = None, *args, **kwargs) -> tuple[dict[str, np.ndarray], dict[str, np.ndarray]]:
+        """
+        Input:
+            image: np.ndarray of shape (H, W, 3), uint8
+            task_description: Optional[str], task description; if different from previous task description, policy state is reset
+        Output:
+            raw_action: dict; raw policy action output
+            action: dict; processed action to be sent to the maniskill2 environment, with the following keys:
+                - 'world_vector': np.ndarray of shape (3,), xyz translation of robot end-effector
+                - 'rot_axangle': np.ndarray of shape (3,), axis-angle representation of end-effector rotation
+                - 'gripper': np.ndarray of shape (1,), gripper action
+                - 'terminate_episode': np.ndarray of shape (1,), 1 if episode should be terminated, 0 otherwise
+        """
+        if (
+                task_description != self.task_description
+                or (goal_image is not None and not np.array_equal(goal_image, getattr(self, "goal_image", None)))
+        ):
+            self.reset(task_description=task_description, goal_image=goal_image)
+
+        assert image.dtype == np.uint8
+        image = self._resize_image(image)
+        self._add_image_to_history(image)
+        images, pad_mask = self._obtain_image_history_and_mask()
+        images, pad_mask = images[None], pad_mask[None]
+
+        # we need use a different rng key for each model forward step; this has a large impact on model performance
+        self.rng, key = jax.random.split(self.rng)  # each shape [2,]
+        # print("octo local rng", self.rng, key)
+
+        input_observation = {"image_primary": images, "pad_mask": pad_mask, "timestep_pad_mask": pad_mask}
+        norm_raw_actions, attention_weights = self.model.sample_actions(
+            input_observation,
+            self.task,
+            rng=key,
+        )
+        raw_actions = norm_raw_actions * self.action_std[None] + self.action_mean[None]
+        raw_actions = raw_actions[0]  # remove batch, becoming (action_pred_horizon, action_dim)
+
+        assert raw_actions.shape == (self.pred_action_horizon, 7)
+        if self.action_ensemble:
+            raw_actions = self.action_ensembler.ensemble_action(raw_actions)
+            raw_actions = raw_actions[None]  # [1, 7]
+
+        raw_action = {
+            "world_vector": np.array(raw_actions[0, :3]),
+            "rotation_delta": np.array(raw_actions[0, 3:6]),
+            "open_gripper": np.array(raw_actions[0, 6:7]),  # range [0, 1]; 1 = open; 0 = close
+        }
+
+        # process raw_action to obtain the action to be sent to the maniskill2 environment
+        action = {}
+        action["world_vector"] = raw_action["world_vector"] * self.action_scale
+        action_rotation_delta = np.asarray(raw_action["rotation_delta"], dtype=np.float64)
+        roll, pitch, yaw = action_rotation_delta
+        action_rotation_ax, action_rotation_angle = euler2axangle(roll, pitch, yaw)
+        action_rotation_axangle = action_rotation_ax * action_rotation_angle
+        action["rot_axangle"] = action_rotation_axangle * self.action_scale
+
+        if self.policy_setup == "google_robot":
+            current_gripper_action = raw_action["open_gripper"]
+
+            # This is one of the ways to implement gripper actions; we use an alternative implementation below for consistency with real
+            # gripper_close_commanded = (current_gripper_action < 0.5)
+            # relative_gripper_action = 1 if gripper_close_commanded else -1 # google robot 1 = close; -1 = open
+
+            # # if action represents a change in gripper state and gripper is not already sticky, trigger sticky gripper
+            # if gripper_close_commanded != self.gripper_is_closed and not self.sticky_action_is_on:
+            #     self.sticky_action_is_on = True
+            #     self.sticky_gripper_action = relative_gripper_action
+
+            # if self.sticky_action_is_on:
+            #     self.gripper_action_repeat += 1
+            #     relative_gripper_action = self.sticky_gripper_action
+
+            # if self.gripper_action_repeat == self.sticky_gripper_num_repeat:
+            #     self.gripper_is_closed = (self.sticky_gripper_action > 0)
+            #     self.sticky_action_is_on = False
+            #     self.gripper_action_repeat = 0
+
+            # action['gripper'] = np.array([relative_gripper_action])
+
+            # alternative implementation
+            if self.previous_gripper_action is None:
+                relative_gripper_action = np.array([0])
+            else:
+                relative_gripper_action = (
+                    self.previous_gripper_action - current_gripper_action
+                )  # google robot 1 = close; -1 = open
+            self.previous_gripper_action = current_gripper_action
+
+            if np.abs(relative_gripper_action) > 0.5 and self.sticky_action_is_on is False:
+                self.sticky_action_is_on = True
+                self.sticky_gripper_action = relative_gripper_action
+
+            if self.sticky_action_is_on:
+                self.gripper_action_repeat += 1
+                relative_gripper_action = self.sticky_gripper_action
+
+            if self.gripper_action_repeat == self.sticky_gripper_num_repeat:
+                self.sticky_action_is_on = False
+                self.gripper_action_repeat = 0
+                self.sticky_gripper_action = 0.0
+
+            action["gripper"] = relative_gripper_action
+
+        elif self.policy_setup == "widowx_bridge":
+            action["gripper"] = (
+                2.0 * (raw_action["open_gripper"] > 0.5) - 1.0
+            )  # binarize gripper action to 1 (open) and -1 (close)
+
+        action["terminate_episode"] = np.array([0.0])
+
+        return raw_action, action, attention_weights
+
 
     def step(self, image: np.ndarray, task_description: Optional[str] = None, *args, **kwargs) -> tuple[dict[str, np.ndarray], dict[str, np.ndarray]]:
         """
